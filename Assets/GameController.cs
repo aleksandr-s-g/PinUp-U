5d69d07fff19c7f9b88cf6ac7baf7fc275929f1f
Ivan Pokatilov <ivan.tul@gmail.com>
2024-01-26 20:22:08 +0300

    Разделил и почистил 2 контроллера. Пытался починить ХУД

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -13,7 +13,7 @@ public class GameController : MonoBehaviour
     public float loseDistance = 30f;
     int currentScore;
     int currentCoins;
-    int loadedScore;
+    
     SaveManager saveManager;
     HUD hud;
    
@@ -30,12 +30,12 @@ public class GameController : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 240;
-      //  hud = HUD.GetComponent<HUD>();
+        hud = HUD.GetComponent<HUD>();
         LosePanel.SetActive(false);
         saveManager = SaveManager.GetComponent<SaveManager>();
         currentScore = 0;
-        currentCoins = saveManager.getCoins();;
-        loadedScore = saveManager.getScores();
+        currentCoins = saveManager.getCoins();
+        
     }
     public void CoinCollected()
     {
@@ -48,14 +48,15 @@ public class GameController : MonoBehaviour
     {
         if ((int)Ball.transform.position.y > currentScore)
         {
-            currentScore = (int)Ball.transform.position.y;
-            saveManager.setScores(currentScore + loadedScore);
+            currentScore = (int)Ball.transform.position.y;            
         }
         if (Ball.transform.position.y < Camera.transform.position.y-loseDistance)
         {
             LosePanel.SetActive(true);
         }
-        //hud.SetScores(currentScore + loadedScore);
-       // hud.SetCoins(currentCoins);
+        // hud.SetScores(currentScore);
+        // hud.SetCoins(currentCoins);
+       // Debug.Log(currentScore);
+       // Debug.Log(currentCoins);
     }
 }
--- Assets/HUD Race.cs
+++ Assets/HUD Race.cs
@@ -1,21 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class HUDRace : MonoBehaviour
 {
+    public TextMeshProUGUI scoresLabel;
+    public TextMeshProUGUI coinsLabel;
+    public TextMeshProUGUI fPSLabel;
     // Start is called before the first frame update
+
+    int m_frameCounter = 0;
+    float m_timeCounter = 0.0f;
+    float m_lastFramerate = 0.0f;
+    public float m_refreshTime = 0.5f;
     void Start()
     {
-
+        scoresLabel.text = "888";
+        //Debug.Log("hud started");
     }
 
     // Update is called once per frame
     void Update()
-    {
 
+    {
+        // Debug.Log("hud upd");
+        if (m_timeCounter < m_refreshTime)
+        {
+            m_timeCounter += Time.deltaTime;
+            m_frameCounter++;
+
+        }
+        else
+        {
+            //This code will break if you set your m_refreshTime to 0, which makes no sense.
+            m_lastFramerate = (float)m_frameCounter / m_timeCounter;
+            m_frameCounter = 0;
+            m_timeCounter = 0.0f;
+        }
+        ShowFPS();
     }
+
+    public void SetScores(int scores)
+    {
+        scoresLabel.text = scores.ToString();
+    }
+    public void SetCoins(int coins)
+    {
+        coinsLabel.text = coins.ToString();
+    }
+    public void ShowFPS()
+    {
+        fPSLabel.text = ((int)m_lastFramerate).ToString();
+    }
+
     public void onBackButtonClicked()
     {
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
