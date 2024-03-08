77017494738152cae9e760454d2353cb7c839eab
Aleksandr Gavrilov <asegavrilov@avito.ru>
2024-03-08 12:52:09 +0400

    sent first fb app event

--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
+using Facebook.Unity;
 
 public class MainController : MonoBehaviour
 {
@@ -15,6 +16,17 @@ public class MainController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Start");
+        Debug.Log("FB.IsInitialized1: " + FB.IsInitialized);
+        if (!FB.IsInitialized) {
+        // Initialize the Facebook SDK
+        FB.Init(InitCallback, OnHideUnity);
+        } else {
+        // Already initialized, signal an app activation App Event
+        FB.ActivateApp(); 
+        }
+        
+
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
         
     }
@@ -48,4 +60,37 @@ public class MainController : MonoBehaviour
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
         SceneManager.UnloadSceneAsync(gameMode);
     }
+
+    private void InitCallback ()
+{
+    if (FB.IsInitialized) {
+        // Signal an app activation App Event
+        FB.ActivateApp();
+        // Continue with Facebook SDK
+        // ...
+    } else {
+        Debug.Log("Failed to Initialize the Facebook SDK");
+    }
+    Debug.Log("FB.IsInitialized2: " + FB.IsInitialized);
+    var tutParams = new Dictionary<string, object>();
+    tutParams[AppEventParameterName.ContentID] = "pinup-u-started";
+    tutParams[AppEventParameterName.Description] = "some event desctiption";
+    tutParams[AppEventParameterName.Success] = "1";
+
+    FB.LogAppEvent (
+        AppEventName.CompletedTutorial,
+        parameters: tutParams
+    );
+}
+
+private void OnHideUnity (bool isGameShown)
+{
+    if (!isGameShown) {
+        // Pause the game - we will need to hide
+        Time.timeScale = 0;
+    } else {
+        // Resume the game - we're getting focus again
+        Time.timeScale = 1;
+    }
+}
 }
