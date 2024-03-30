6e92194c5d70b6f52bbc4a6c59ee8d6e4959b6af
Aleksandr Gavrilov <asegavrilov@avito.ru>
2024-03-30 19:06:53 +0400

    clear camera debug logs

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -11,22 +11,10 @@ public class CameraScript : MonoBehaviour
     void Start()
     {
         float orthoSize = sceneWidth * Screen.height/Screen.width*0.5f;
-        Debug.Log("Screen.height: " + Screen.height);
-        Debug.Log("Screen.width: " + Screen.width);
-        Debug.Log("Screen sceneWidth " + sceneWidth);
-        Debug.Log("Screen orthoSize: " + orthoSize);
         float singleBlockPxSize = Screen.width / sceneWidth;
         cameraStartpositionY = Screen.height*0.5f/singleBlockPxSize;
         cameraStartpositionX = 5f;
         Camera.allCameras[0].orthographicSize = orthoSize;
-        //Camera.main.orthographicSize = orthoSize;
-        //foreach (Camera c in Camera.allCameras)
-        //{
-        //    c.orthographicSize = orthoSize;
-        //}
-        //Debug.Log("Screen Camera.current.orthographicSize: " + Camera.current.orthographicSize);
-        //Debug.Log("Screen Camera.allCamerasCount: "+Camera.allCamerasCount);
-        //Debug.Log("Screen Camera.allCameras: " + Camera.allCameras[0]);
         transform.position = new Vector3(cameraStartpositionX,cameraStartpositionY,-1);
     }
 
